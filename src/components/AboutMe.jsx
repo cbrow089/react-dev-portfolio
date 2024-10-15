942210ee74b111358dadcf84d6faa1f9a1b1f54b
cbrow089 <cbrow089@yahoo.com>
2024-10-15 11:28:20 -0400

    added readme and link to deployed app

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,7 +5,7 @@ const AboutMe = () => {
     <section>
       <div className="text-div">
         <h2>About Me</h2>
-        <img id = "image" src="src/assets/images/AvatarOfMe.jpg" alt="Coleman Brown" />
+        <img id = "image" src="/images/AvatarOfMe.jpg" alt="Coleman Brown" />
         <p>Welcome to my portfolio website! I'm Coleman Brown, an upcoming Web Developer with a passion for building innovative and user-friendly applications.</p>
         <p>I have a few years of experience working with different coding languages, and I've worked on a variety of projects that showcase my skills and expertise.</p>
         <p>When I'm not coding, I enjoy video games, cooking, and building computers.</p>
